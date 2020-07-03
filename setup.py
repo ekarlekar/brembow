f651ca31f84ce84b3d9daa7b5adc6532357f666b
Esha Karlekar <esha.karlekar@gmail.com>
2020-07-03 12:26:52 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+        name='brembow',
+        version='0.1',
+        url='https://github.com/ekarlekar/brembow',
+        author='Esha Karlekar',
+        author_email='esha.karlekar@gmail.com',
+        license='MIT',
+        packages=[
+            'brembow'
+        ]
+)
