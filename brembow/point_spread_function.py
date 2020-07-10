790a0330dce18a8b832aa15ab077d7d79d4a37c6
Esha Karlekar <esha.karlekar@gmail.com>
2020-07-09 23:54:37 -0400

    Add function get_radius

--- brembow/point_spread_function.py
+++ brembow/point_spread_function.py
@@ -1,3 +1,4 @@
+import numpy as np
 from scipy.ndimage import gaussian_filter
 
 
@@ -8,11 +9,14 @@ class PointSpreadFunction:
     def apply_psf(self, *args, **kwargs):
         pass
 
+    def get_radius(self):
+        '''Return the radius (in world units) of this PSF'''
+        pass
 
 class GaussianPSF(PointSpreadFunction):
     def __init__(self, intensity, sigma):
         self.intensity = intensity
-        self.sigma = sigma
+        self.sigma = np.array(sigma)
 
     def apply_psf(self, point_image):
         gaussian_filter(
@@ -21,3 +25,6 @@ class GaussianPSF(PointSpreadFunction):
             output=point_image,
             truncate=3.0)
         point_image *= self.intensity
+
+    def get_radius(self):
+        return 3.0 * self.sigma
