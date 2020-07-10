2faf9e0d879f55c0dbbb0037853549f5f2b3aa04
Esha Karlekar <esha.karlekar@gmail.com>
2020-07-10 00:02:48 -0400

    Add filter_locations method to brembow package

--- brembow/__init__.py
+++ brembow/__init__.py
@@ -1,4 +1,4 @@
-from .render import simulate_random_cages, simulate_cages, render_cage_distribution, render_cage, render_points
+from .render import filter_locations, simulate_random_cages, simulate_cages, render_cage_distribution, render_cage, render_points
 from .cage import Cage
 from .volume import Volume
 from .point_spread_function import PointSpreadFunction, GaussianPSF
